21bf2c6e55b22afce9f56718fb76b670d9aef571
poontamilm07 <poontamil03@gmail.com>
2026-06-18 14:55:31 +0530

    Update AuthController.java

--- civicbackend/src/main/java/com/civicissues/controller/AuthController.java
+++ civicbackend/src/main/java/com/civicissues/controller/AuthController.java
@@ -11,7 +11,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "https://crowd-sourced-civic-issues-reportin.vercel.app"
+})
 public class AuthController {
 
     @Autowired
